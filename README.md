9abd92dc8e418575c4deb512d0d8f9d397fccbea
Linus <linus@freli.se>
2025-06-14 14:38:02 +0200

    Update to Odin 2025-06 and add ols
    Pretty major update.
    
    - Rename: odin-syslib-flake -> odin-flake
      - Kept an alias, should not break old use
    - Update to Odin 2025-06
    - Derive from llvmPackages.stdenv
      - Missed it before, 2025-06 no longer let me get away with it
    - Add ols package, so it can use same Odin version

--- README.md
+++ README.md
@@ -1,42 +1,54 @@
-# odin-syslib-flake
+# odin-flake
+
+Flake to build and install Odin in NixOS.  
+
+It includes:
+
+- Some patches for batteries that did not fit
+- Package for ols, so it can use the same Odin version
 
 ## Disclaimer!
 
 This is a flake for my own convenience.  
-It might work for you. Or not.
+Breaking changes might happen.
 
-I'm likely to make breaking changes.  
-Both intentionally and unintentionally.
-
-**You use this flake on your own risk!**
-
-If you *do* use it, I highly recommend you lock to a specific commit.  
-And check the commit history before you update.
-
-## Status
-
-I've only used Raylib myself, so that is the only one patched at the moment.
-
-Patched and tested:
-- Raylib
-
-Patched but not tested:
-- N/A
-
-Not patched:
-- Everything else
+If you want to try it out, I recommend you pin it to a specific revision.  
+And always check the changes before you update!
 
 ## Why?
 
-NixOS is special. It does not come with a Linux FHS and it does not support dynamic linking.
+NixOS is special. It does not come with a Linux FHS and it does not support
+dynamic linking.
 
 Which means the included vendor libraries in Odin does not work very well.
 
-For Raylib, I needed a workaround for it to find X11 libraries, or I could not open a window.  
-And then it failed to initialize audio, so I could not play sounds. I could not find a workaround for that.
+## Status
 
-Downloading Odin to a local folder, I realized I could just change the vendor library to load Raylib from system instead of from the Odin package.  
-So I created a flake to apply that patch and install it in the system.
+I've only used a few libraries:
+
+- raylib
+  - Needed patching to use `system`
+  - NixOS 25.05 comes with raylib 5.5
+    - Matches the bindings
+- sdl3
+  - Already use `system`
+  - NixOS 25.05 comes with sdl3 3.2.10
+    - Matches the bindings
+- sdl2
+  - Already use `system`
+  - NixOS 25.05 comes with sdl2 using sdl2-compat, which uses sdl3 behind the scenes
+    - Does not match the bindings, which uses 2.0.16
+    - But it worked for my very limited test
+    - For closer match, use an older nixpkgs
+      - 2.0.16 never made it into nixpkgs
+      - 2.0.22 is probably good enough
+
+A lot of the other packages already use `system` and might work just fine, as long
+as you have the libraries installed.
+
+Some will need patching.  
+I'll do them if/when I need them and write code to use them, so I can test.  
+Or if someone supplies code to test with.
 
 ## Usage
 
@@ -45,17 +57,49 @@ Requires your NixOS to have flakes enabled.
 Add this to your inputs:
 ￼￼￼
 ``` nix
-inputs.odin-syslib-flake.url = "github:freli/odin-syslib-flake?rev=COMMIT_SHA";
+inputs.odin-flake.url = "github:freli/odin-flake?rev=COMMIT_SHA";
 ```
 ￼￼
-And then you can add it to your system with
+And then you can add it to your shell with
 
 ``` nix
-environment.systemPackages = with pkgs; [
-  inputs.odin-syslib-flake.packages."${system}".odin-syslib
+inputs.odin-flake.packages."${system}".odin
+inputs.odin-flake.packages."${system}".ols
 ```
 
-And don't forget to also install the libraries you want to use!  
-Check which version of Odin is used in the flake, go to [Tags · odin-lang/Odin](https://github.com/odin-lang/Odin/tags),
-go into the matching version and dive into the vendors folder and see what versions they expect.  
-Hopefully that matches what you will get from NixOS by default, otherwise you are on your own to figure out how to get the correct one.
+And don't forget to also install the libraries you want to use!
+
+## odin-flake-test
+
+There is a companion repository: `odin-flake-test`  
+I use it to test things.
+
+It contains a flake for setting up a developer environment, with examples
+using explicit nixpkgs versions to get library versions matching the bindings.
+
+## Implementation notes
+
+### buildPhase
+
+There is no `buildPhase` for the Odin package.
+
+As explained in [Nixpkgs Reference Manual - Standard Environment - Phases](https://nixos.org/manual/nixpkgs/stable/#sec-stdenv-phases),
+the default build phase will run `make <buildFlags>`, if the package contains
+a `Makefile`.  
+Odin does and it will run `build_odin.sh`.
+
+### Replacing versions
+
+Both Odin and ols try to set the version from `.git` but we don't have that.
+
+For Odin, that means it will fall back to current year-month when building.  
+In ols, it just fails and leave version empty.
+
+In both cases, it's better to use the version we set in the flake.
+
+For Odin, `-custom-nixos` is also added to the version, to indicate that it's
+not a stock build.
+
+## License
+
+MIT No Attribution.
--- flake.nix
+++ flake.nix
@@ -1,104 +1,138 @@
 {
-    description = "Odin lang patched to use system installed libraries on NixOS";
+  description = "Odin lang patched to use system installed libraries on NixOS";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-        #nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    };
+  # inputs = {
+  #   nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  # };
 
-    outputs = { self, nixpkgs, ... }: 
-        let
-            # Version and hash needs to be changed, when updating to newer Odin lang version
-            version = "dev-2025-04";
-            hash = "sha256-dVC7MgaNdgKy3X9OE5ZcNCPnuDwqXszX9iAoUglfz2k=";
+  outputs = { self, nixpkgs, ... }: 
+    let
+        supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+        forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+        nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+    in
+    {
+      overlay = final: prev: {
 
-            supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-            forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-            nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
-        in
-        {
-            overlay = final: prev: {
+        odin = with final; llvmPackages.stdenv.mkDerivation rec {
+          pname = "odin";
+          version = "dev-2025-06";
 
-                odin-syslib = with final; stdenv.mkDerivation rec {
-                    pname = "odin-syslib";
-                    inherit version;
+          src = pkgs.fetchFromGitHub {
+            owner = "odin-lang";
+            repo = "Odin";
+            rev = "${version}";
+            hash = "sha256-Dhy62+ccIjXUL/lK8IQ+vvGEsTrd153tPp4WIdl3rh4=";
+          };
 
-                    src = pkgs.fetchFromGitHub {
-                        owner = "odin-lang";
-                        repo = "Odin";
-                        rev = "${version}";
-                        hash = "${hash}";
-                    };
+          LLVM_CONFIG = "${llvmPackages.llvm.dev}/bin/llvm-config";
 
-                    LLVM_CONFIG = "${llvmPackages.llvm.dev}/bin/llvm-config";
+          dontConfigure = true;
 
-                    dontConfigure = true;
+          buildFlags = ["release"];
 
-                    buildFlags = ["release"];
+          nativeBuildInputs = [
+            llvmPackages.bintools
+            llvmPackages.llvm
+            llvmPackages.clang
+            llvmPackages.lld
+            makeBinaryWrapper
+            which
+          ];
 
-                    nativeBuildInputs = [
-                        llvmPackages.bintools
-                        llvmPackages.llvm
-                        llvmPackages.clang
-                        llvmPackages.lld
-                        makeBinaryWrapper
-                        which
-                    ];
+          patches = [
+            ./raylib.patch
+          ];
 
-                    patches = [./odin-system-raylib.patch];
+          postPatch = ''
+            patchShebangs build_odin.sh
+            sed -i 's/\\"dev-\$GIT_DATE\\"/\\"${version}-custom-nixos\\"/' build_odin.sh
+          '';
 
-                    postPatch = ''
-                        patchShebangs build_odin.sh
-                        sed -i 's/\\"dev-\$(date +"%Y-%m")\\"/\\"${version}-syslib\\"/' build_odin.sh
-                    '';
+          installPhase = ''
+            runHook preInstall
 
-                    #nativeBuildInputs = [ autoreconfHook ];
+            mkdir -p $out/bin
+            cp odin $out/bin/odin
 
-                    installPhase = ''
-                        runHook preInstall
+            mkdir -p $out/share
+            cp -r {base,core,shared,vendor} $out/share
 
-                        mkdir -p $out/bin
-                        cp odin $out/bin/odin
+            make -C "$out/share/vendor/cgltf/src/"
+            make -C "$out/share/vendor/stb/src/"
+            make -C "$out/share/vendor/miniaudio/src/"
 
-                        mkdir -p $out/share
-                        cp -r {base,core,shared,vendor} $out/share
+            wrapProgram $out/bin/odin \
+              --set-default ODIN_ROOT $out/share \
+              --prefix PATH : ${
+              lib.makeBinPath (
+                with llvmPackages; [
+                  bintools
+                  llvm
+                  clang
+                  lld
+                ]
+              )
+            }
 
-                        make -C "$out/share/vendor/cgltf/src/"
-                        make -C "$out/share/vendor/stb/src/"
-                        make -C "$out/share/vendor/miniaudio/src/"
-
-                        wrapProgram $out/bin/odin \
-                            --set-default ODIN_ROOT $out/share \
-                            --prefix PATH : ${
-                            lib.makeBinPath (
-                                with llvmPackages; [
-                                    bintools
-                                    llvm
-                                    clang
-                                    lld
-                                ]
-                            )
-                        }
-
-                        runHook postInstall
-                    '';
-                };
-
-            };
-
-            # Provide some binary packages for selected system types.
-            packages = forAllSystems (system:
-            {
-                inherit (nixpkgsFor.${system}) odin-syslib;
-            });
-
-            # The default package for 'nix build'. This makes sense if the
-            # flake provides only one package or there is a clear "main"
-            # package.
-            defaultPackage = forAllSystems (system: self.packages.${system}.odin-syslib);
-
+            runHook postInstall
+          '';
         };
-    # Indent matching `output`, we are not missing a }
+
+        # For compatibility with old flake
+        odin-syslib = final.odin;
+
+        ols = with final; stdenv.mkDerivation rec {
+          pname = "ols";
+          version = "0-unstable-2025-06-03-8bcc43f";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "DanielGavin";
+            repo = "ols";
+            rev = "8bcc43ff70994bcad692075bbe6b14f37ebe8c56";
+            hash = "sha256-OaH9hjim8XGqTkEdxPsfFWk5d1XAMD4R09BYsDbmgmY=";
+          };
+
+          postPatch = ''
+            patchShebangs build.sh odinfmt.sh
+            sed -i 's/^version.*/version="${version}"/' build.sh
+          '';
+
+          nativeBuildInputs = [ makeBinaryWrapper ];
+          buildInputs = [ odin ];
+
+          buildPhase = ''
+            runHook preBuild
+
+            ./build.sh && ./odinfmt.sh
+
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/bin
+            cp ols odinfmt $out/bin/
+
+            wrapProgram $out/bin/ols \
+              --set-default ODIN_ROOT ${odin}/share
+
+            runHook postInstall
+          '';
+        };
+      };
+
+      # Expose packages for selected systems
+      packages = forAllSystems (system:
+      {
+          inherit (nixpkgsFor.${system}) odin ols odin-syslib;
+      });
+
+      # Default package for 'nix build'
+      defaultPackage = forAllSystems (system: self.packages.${system}.odin);
+    };
+  # Indent matching `output`, we are not missing a }
 }
 
-# vim: noai:ts=4:sw=4
+# vim: noai:ts=2:sw=2
